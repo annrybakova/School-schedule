eaf5650bb5c8fe03ba821ac4ce1322e1dffffdd3
Anna Rybakova <anna.rybakova.hse@gmail.com>
2025-09-25 16:23:43 -0400

    claassubject updt

--- src/main/java/com/solvd/school/dao/mybatisimpl/ClassSubjectDAO.java
+++ src/main/java/com/solvd/school/dao/mybatisimpl/ClassSubjectDAO.java
@@ -2,11 +2,9 @@ package com.solvd.school.dao.mybatisimpl;
 
 import com.solvd.school.dao.interfaces.IClassSubjectDAO;
 import com.solvd.school.model.ClassSubject;
-import org.apache.ibatis.session.SqlSession;
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import java.util.List;
+import org.apache.ibatis.session.*;
+import org.apache.logging.log4j.*;
+import java.util.*;
 
 public class ClassSubjectDAO implements IClassSubjectDAO {
     private static final Logger log = LogManager.getLogger(ClassSubjectDAO.class);
@@ -18,38 +16,39 @@ public class ClassSubjectDAO implements IClassSubjectDAO {
     }
 
     @Override
-    public List<ClassSubject> getAll() {
-        try (SqlSession s = sf.openSession()) {
-            return s.selectList(NS + ".getAll");
-        }
-    }
-
-    @Override
-    public Integer getRandomSubjectIdForClass(int classId) {
-        try (SqlSession s = sf.openSession()) {
-            return s.selectOne(NS + ".getRandomSubjectIdForClass", classId);
-        }
-    }
-
-    @Override
     public ClassSubject getById(int id) {
         try (SqlSession s = sf.openSession()) {
             return s.selectOne(NS + ".getById", id);
         }
     }
 
-    @Override
-    public void insert(ClassSubject entity) {
-        try (SqlSession s = sf.openSession(true)) {
-            s.insert(NS + ".insert", entity);
-            log.info("Inserted GA params: {}", entity);
+    public List<ClassSubject> getByClassId(int classId) {
+        try (SqlSession s = sf.openSession()) {
+            return s.selectList(NS + ".getByClassId", classId);
+        }
+    }
+
+    public ClassSubject getByClassAndSubject(int classId, int subjectId) {
+        Map<String, Object> p = new HashMap<>();
+        p.put("classId", classId);
+        p.put("subjectId", subjectId);
+        try (SqlSession s = sf.openSession()) {
+            return s.selectOne(NS + ".getByClassAndSubject", p);
         }
     }
 
     @Override
-    public void update(ClassSubject entity) {
+    public void insert(ClassSubject cs) {
         try (SqlSession s = sf.openSession(true)) {
-            s.update(NS + ".update", entity);
+            s.insert(NS + ".insert", cs);
+            log.info("Inserted class_subject: {}", cs);
+        }
+    }
+
+    @Override
+    public void update(ClassSubject cs) {
+        try (SqlSession s = sf.openSession(true)) {
+            s.update(NS + ".update", cs);
         }
     }
 
@@ -57,6 +56,18 @@ public class ClassSubjectDAO implements IClassSubjectDAO {
     public void delete(int id) {
         try (SqlSession s = sf.openSession(true)) {
             s.delete(NS + ".delete", id);
+        }
+    }
+
+    @Override
+    public List<ClassSubject> getAll() {
+        return List.of();
+    }
+
+    @Override
+    public Integer getRandomSubjectIdForClass(int classId) {
+        try (SqlSession s = sf.openSession()){
+            return s.selectOne(NS + ".getRandomSubjectIdForClass", classId);
         }
     }
 }
