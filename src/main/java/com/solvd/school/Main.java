bbba31c80a20e0bce264c79a88a516dd4830e45d
Anna Rybakova <anna.rybakova.hse@gmail.com>
2025-09-25 14:52:21 -0400

    main updt

--- src/main/java/com/solvd/school/Main.java
+++ src/main/java/com/solvd/school/Main.java
@@ -1,5 +1,11 @@
 package com.solvd.school;
 
+import com.solvd.school.dao.mybatisimpl.ClassDAO;
+import com.solvd.school.dao.mybatisimpl.ClassroomsDAO;
+import com.solvd.school.dao.mybatisimpl.LessonDAO;
+import com.solvd.school.dao.mybatisimpl.SpecialClassroomDAO;
+import com.solvd.school.dao.mybatisimpl.SubjectDAO;
+import com.solvd.school.dao.mybatisimpl.TeacherDAO;
 import com.solvd.school.generator.weeklyScheduleGenerator.impl.RandomWeeklyScheduleGenerator;
 import com.solvd.school.generator.weeklyScheduleGenerator.interfaces.IWeeklyScheduleGenerator;
 import com.solvd.school.model.Lesson;
@@ -7,8 +13,20 @@ import com.solvd.school.model.SchoolClass;
 import com.solvd.school.model.schedule.DailySchedule;
 import com.solvd.school.model.schedule.WeeklySchedule;
 import com.solvd.school.service.impl.ClassServiceImpl;
+import com.solvd.school.service.impl.ClassroomServiceImpl;
+import com.solvd.school.service.impl.LessonServiceImpl;
+import com.solvd.school.service.impl.SubjectServiceImpl;
+import com.solvd.school.service.impl.TeacherServiceImpl;
+import com.solvd.school.service.impl.ValidationServiceImpl;
 import com.solvd.school.service.interfaces.IClassService;
+import com.solvd.school.service.interfaces.IClassroomService;
+import com.solvd.school.service.interfaces.ILessonService;
+import com.solvd.school.service.interfaces.ISubjectService;
+import com.solvd.school.service.interfaces.ITeacherService;
+import com.solvd.school.service.interfaces.IValidationService;
+import com.solvd.school.util.MyBatisUtil;
 
+import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -20,7 +38,27 @@ public class Main {
 
     public static void main(String[] args) {
 
-        List<SchoolClass> allClasses = getAllClasses();
+        SqlSessionFactory sqlSessionFactory = MyBatisUtil.getSqlSessionFactory();
+
+        LessonDAO lessonDAO = new LessonDAO(sqlSessionFactory);
+        TeacherDAO teacherDAO = new TeacherDAO(sqlSessionFactory);
+        SubjectDAO subjectDAO = new SubjectDAO(sqlSessionFactory);
+        ClassroomsDAO classroomDAO = new ClassroomsDAO(sqlSessionFactory);
+        ClassDAO classDAO = new ClassDAO(sqlSessionFactory);
+        SpecialClassroomDAO specialClassroomDAO = new SpecialClassroomDAO(sqlSessionFactory);
+
+        // Service Initialization
+        ILessonService lessonService = new LessonServiceImpl(lessonDAO);
+        ITeacherService teacherService = new TeacherServiceImpl(teacherDAO, lessonDAO);
+        ISubjectService subjectService = new SubjectServiceImpl(subjectDAO);
+        IClassroomService classroomService = new ClassroomServiceImpl(classroomDAO, lessonDAO, specialClassroomDAO);
+        IClassService classService = new ClassServiceImpl(classDAO);
+
+        IValidationService validationService = new ValidationServiceImpl(
+                teacherService, subjectService, classroomService, classService
+        );
+
+         List<SchoolClass> allClasses = getAllClasses(classService);
 
         // for (SchoolClass schoolClass : allClasses) {
         // System.out.println(schoolClass);
@@ -40,8 +78,8 @@ public class Main {
         }
     }
 
-    private static List<SchoolClass> getAllClasses() {
-        IClassService classService = new ClassServiceImpl();
+    private static List<SchoolClass> getAllClasses(IClassService classService) {
+        // IClassService classService = new ClassServiceImpl();
         return classService.getAllClasses();
     }
 
--- src/main/java/com/solvd/school/service/impl/ClassServiceImpl.java
+++ src/main/java/com/solvd/school/service/impl/ClassServiceImpl.java
@@ -1,10 +1,8 @@
 package com.solvd.school.service.impl;
 
 import com.solvd.school.dao.interfaces.IClassDAO;
-import com.solvd.school.dao.mybatisimpl.ClassDAO;
 import com.solvd.school.model.SchoolClass;
 import com.solvd.school.service.interfaces.IClassService;
-import com.solvd.school.util.MyBatisUtil;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -12,7 +10,11 @@ import java.util.List;
 
 public class ClassServiceImpl implements IClassService {
     private static final Logger logger = LogManager.getLogger(ClassServiceImpl.class);
-    private static final IClassDAO classDAO = new ClassDAO(MyBatisUtil.getSqlSessionFactory());
+    private final IClassDAO classDAO;
+
+    public ClassServiceImpl(IClassDAO classDAO) {
+        this.classDAO = classDAO;
+    }
 
     @Override
     public List<SchoolClass> getAllClasses() {
